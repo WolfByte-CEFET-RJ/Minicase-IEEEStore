9e183f076fc102b3241c934cde09a743396df57b
Cap1vas <gat3105@gmail.com>
2025-01-06 21:35:40 -0300

    Subindo arquivos das migrations cliente e administrador

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,19 @@
-const { configDotenv } = require("dotenv");
-configDotenv();
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      host: process.env.KNEX_HOST,
-      port: process.env.KNEX_PORT,
-      user: process.env.KNEX_USERNAME,
-      password: process.env.KNEX_PASSWORD,
-      database: process.env.KNEX_DATABASE,
-    },
-  });
-  
+const path = require('path');
+require('dotenv').config();
+require('dotenv').config({path: '../../.env'});
+console.log(`SE LIGA ${process.env.KNEX_DATABASE}`);
+
+module.exports = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.KNEX_HOST,
+    user: process.env.KNEX_USERNAME,
+    password: process.env.KNEX_PASSWORD,
+    database: process.env.KNEX_DATABASE,
+    port: process.env.KNEX_PORT,
+  },
+  migrations: {
+    directory: path.resolve(__dirname, './db/migrations'),
+    tableName: 'knex_migrations',
+  },
+};
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ configDotenv();
 const app = express();
 app.use(homeRouter) 
 
+console.log('Host:', process.env.KNEX_HOST);
+console.log('Port:', process.env.KNEX_PORT);
+console.log('User:', process.env.KNEX_USERNAME);
+console.log('Password:', process.env.KNEX_PASSWORD);
+console.log('Database:', process.env.KNEX_DATABASE);
+
 app.listen(process.env.PORT,'0.0.0.0',()=>{
     console.log("Servidor subiu!!");
 })
