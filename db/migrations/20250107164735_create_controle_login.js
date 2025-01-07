60fa631d8c59e1c1c178870c547fd65283beb626
Erick <135640111+erick1-618@users.noreply.github.com>
2025-01-07 14:07:26 -0300

    feat: add create_controle_login, create_equipe

---
+++ db/migrations/20250107164735_create_controle_login.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('controle_login', (table) => {
+    table.increments("id_controle").notNullable().primary();
+    table.integer('id_pessoa').notNullable().unsigned();
+    table.timestamp('hora_login').defaultTo(knex.fn.now());
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
