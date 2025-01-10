c036de2f026192947f1d32dbd2adc0610d8f5127
LucasMontenegroM <lucasmontereira.com>
2025-01-10 14:56:28 -0300

    Mudanca no path do knexfile que causava erros

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
 const path = require('path');
-require('dotenv').config({path: '../../.env'});
+require('dotenv').config();
 
 console.log('KNEX_HOST:', process.env.KNEX_HOST);
 
 module.exports = {
   client: 'mysql2',
-git commit -m "Mensagem do commit explicando as alterações"
+  connection: {
+    host: process.env.KNEX_HOST,
+    user: process.env.KNEX_USERNAME,
+    password: process.env.KNEX_PASSWORD,
+    database: process.env.KNEX_DATABASE,
+    port: process.env.KNEX_PORT,
+  },
   migrations: {
     directory: path.resolve(__dirname, './src/db/migrations'),
     tableName: 'knex_migrations',
