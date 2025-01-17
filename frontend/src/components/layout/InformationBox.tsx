193b89b43fa590c6a355e581c733c69822b547b4
Marcos Samuel <marcs262002@gmail.com>
2025-01-17 11:36:15 -0300

    Alterando tamanho de formulário do login

--- frontend/src/components/layout/InformationBox.tsx
+++ frontend/src/components/layout/InformationBox.tsx
@@ -7,7 +7,7 @@ interface InformationBoxProps {
 
 export default function InformationBox(props: InformationBoxProps) {
     return (
-        <section className={`layout-boxed mt-20 bg-amber-50 rounded-3xl flex flex-col gap-6 py-10 ${props.className}`}>
+        <section className={`mt-20 bg-amber-50 rounded-3xl flex flex-col gap-6 py-10 ${props.className ? props.className : 'layout-boxed'}`}>
             {props.children}
         </section>
     )
