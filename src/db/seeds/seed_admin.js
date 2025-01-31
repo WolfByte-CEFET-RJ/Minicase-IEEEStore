6aabfb56c8a021de33e565cc7ccae9190d027615
Daniel Mota de Souza <Danielmotads007@gmail.com>
2025-01-31 17:56:09 -0300

    hasheei e botei as paradas no .env

--- src/db/seeds/seed_admin.js
+++ src/db/seeds/seed_admin.js
@@ -1,8 +1,10 @@
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
- */
+*/
 exports.seed = async function(knex) {
+  const bcrypt = require('bcrypt');
   // Deletes ALL existing entries
   await knex('administrador').del()
   await knex('administrador').insert([
@@ -10,25 +12,9 @@ exports.seed = async function(knex) {
       id: 1,
       nome: 'Carlinhos', 
       cargo: JSON.stringify('secretario'), 
-      cpf: '11111111111',
-      telefone: "21123454321", 
-      senha: '1233221'
-    },
-    {
-      id: 2,
-      nome: 'Rony', 
-      cargo: JSON.stringify('secretario'), 
-      cpf: '10987654321',
-      telefone: "21123454323", 
-      senha: '1233221'
-    },
-    {
-      id: 3,
-      nome: 'Gilberto', 
-      cargo: JSON.stringify('secretario'), 
-      cpf: '12345678910',
-      telefone: "21123454322", 
-      senha: '1233221'
+      cpf: process.env.CPF_ADM,
+      telefone: process.env.TELEFONE_ADM, 
+      senha: await bcrypt.hash(process.env.SENHA_ADM,11)
     },
   ]);
 };
