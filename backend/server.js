e5b6c5a5d695a1bb27f0ee40b3398f9bf73c2230
Daniel Mota de Souza <Danielmotads007@gmail.com>
2025-02-15 18:41:53 -0300

    n sei se resolvi o problema do cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require("express");
 const adminRouter= require("./src/routes/routes.js");
+const cors = require('cors');
 //config.Dotenv();
 const app = express();
 app.use(express.json());
 app.use(adminRouter); 
+app.use(cors())
 
 
 
