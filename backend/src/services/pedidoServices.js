7cd3573b64b982d99acf990650962e0502fceed6
LucasMontenegroM <lucasmontereira.com>
2025-03-28 19:10:32 -0300

    feat: Tratamento de erros no pedido e tabela item melhorados.

--- backend/src/services/pedidoServices.js
+++ backend/src/services/pedidoServices.js
@@ -32,16 +32,40 @@ async function viewAllOrders(){
     }
   }
 const carrinho = {
-    id_user: 1,
+    id_user: 2,
     produtos: [
         { id_produto: 1, quantidade: 2 },
-        { id_produto: 2, quantidade: 1 },
-        { id_produto: 3, quantidade: 3 }
+        { id_produto: 5, quantidade: 2 },
+        { id_produto: 3, quantidade: 2 }
     ],
     preco_final: 250
 };
 async function createOrder({id_usuario,preco_final, metodo_pagamento, comprovante, estado_pedido, mensagem}) {
     try {
+        const usuarioExistente = await knex("cliente").where({id: carrinho.id_user}).first();
+        if(!usuarioExistente){
+            throw new Error("Não existe usuário com esse id no banco");
+        }
+        for (let item of carrinho.produtos) {
+            console.log("Produto sendo verificado:", item);
+            const produto = await knex("produto").where({id: item.id_produto}).first();
+            if (!produto) {
+                throw new Error("Produto não encontrado no banco de dados");
+            }
+            const quantidade = item.quantidade
+            
+            
+            if (quantidade <= 0 ){
+                throw new Error("Produto fora de estoque.");
+            }
+            if (quantidade > produto.quantidade){
+                throw new Error("Há uma quantidade maior de produtos no carrinho que no estoque.")
+            }
+
+            console.log("Produto verificado com sucesso.");
+        }
+       
+
         if (!carrinho.preco_final || !metodo_pagamento || !comprovante) {
             throw new Error("Preencha todos os campos obrigatórios.");
         }
