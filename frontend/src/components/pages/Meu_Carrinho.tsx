fbeaf2b3f6eea2c766dc585c611d11062b765bfb
CelesteBia <124384519+CelesteBia@users.noreply.github.com>
2025-04-12 13:59:57 -0300

    Mudança de design da tela do carrinho
    Adicionei as bordas nos itens do carrinho, arredondei as imagens no lado direito, enquanto no lado esquerda são retas e desabilitei a contagem de subtração quando a quantidade 1 é atingida.

--- frontend/src/components/pages/Meu_Carrinho.tsx
+++ frontend/src/components/pages/Meu_Carrinho.tsx
@@ -18,21 +18,25 @@ export default function Cart() {
         <div>
           <ul>
             {productsCart.map((product) => (
-              <li key={product.id} className="flex justify-between items-center mb-2 p-2 border-b">
+              <li key={product.id} className="flex items-stretch mb-4 border-[6px] border-gray-300 rounded-xl shadow overflow-hidden">
                 <div className="flex items-center">
-                  <img src={product.foto} alt={product.nome} className="w-16 h-16 mr-4" />
-                  <div className="flex flex-col gap-y-2">
+                  <img src={product.foto} alt={product.nome} className="w-32 h-auto object-cover rounded-l-xl mr-4" />
+                  <div className="flex flex-col justify-between p-4 flex-1">
                     <p className="text-2xl font-semibold">{product.nome}</p>
                     <p className="text-lg font-medium">Preço: R${product.preco.toFixed(2)}</p>                
                     <div className="flex items-center gap-5">
                       <p className="text-base">Quantidade:</p>
                       <div className="flex items-center gap-5 border-2 border-[#0D5FAA] rounded-2xl px-5 py-2">
-                        <button
-                          onClick={() => removeProductFromCart(product.id)}
-                          className="bg-gray-200 px-2 rounded-md text-xl font-medium"
-                        >
-                          -
-                        </button>
+                      <button
+                        onClick={() => removeProductFromCart(product.id)}
+                        disabled={product.quantidade === 1}
+                        className={`px-2 rounded-md text-xl font-medium ${
+                          product.quantidade === 1
+                            ? "bg-gray-200 text-gray-400 cursor-not-allowed"
+                            : "bg-gray-200 hover:bg-gray-300"
+                        }`}>
+                        -
+                      </button>
                         <span className="font-medium">{product.quantidade}</span>
                         <button
                           onClick={() => addProductToCart(product)}
